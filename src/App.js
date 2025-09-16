3af4375e89a7ef45eeabb55840bc46aa61f697a8
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-09-15 22:02:32 -0300

    se borro el logotipo de react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo
-        </p>
+        <p>hola mundo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
