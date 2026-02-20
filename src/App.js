c11d84d855c99b2fd39766130a6b758157b8178c
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2026-02-19 22:08:29 -0300

    se añadio fondo blanco

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>hola mundo</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola mundo desde esta mac
-        </a>
       </header>
     </div>
   );
