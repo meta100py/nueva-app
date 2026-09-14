17d9c1e54e474d35ff2cf9b151ace0e50b5d18b3
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2026-09-14 20:53:25 -0300

    added a h1 title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hola mundo</p>
+        <h1>hola mundo</h1>
       </header>
     </div>
   );
